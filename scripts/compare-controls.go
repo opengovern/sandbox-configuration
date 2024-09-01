250a2587a15d50d963d9f7f1461aca96e8905fd6
artaasadi <arta-c@kaytu.io>
2024-09-01 15:56:02 +0200

    fix: check missing child benchmarks

--- scripts/compare-controls.go
+++ scripts/compare-controls.go
@@ -40,15 +40,20 @@ var (
 )
 
 func main() {
-	benchmarksControls := getBenchmarkControls(BenchmarksPath)
-	controlsList := getControls(ControlsPath)
+	benchmarks := getBenchmarks(BenchmarksPath)
+	controlsList, err := getControls(ControlsPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	compareControls(benchmarksControls, controlsList)
+	exitCode := compareBenchmarks(benchmarks, controlsList)
+
+	os.Exit(exitCode)
 }
 
-// getBenchmarkControls parses all benchmark YAML files and returns a list of control IDs
-func getBenchmarkControls(root string) []string {
-	var controls []string
+// getBenchmarks parses all benchmark YAML files and returns a map of benchmarks with control IDs and children
+func getBenchmarks(root string) map[string]Benchmark {
+	benchmarks := make(map[string]Benchmark)
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -56,8 +61,7 @@ func getBenchmarkControls(root string) []string {
 		}
 		if strings.HasSuffix(info.Name(), ".yaml") {
 			benchmark := parseBenchmarkFile(path)
-			controls = append(controls, benchmark.Controls...)
-
+			benchmarks[benchmark.ID] = benchmark
 		}
 		return nil
 	})
@@ -66,27 +70,27 @@ func getBenchmarkControls(root string) []string {
 		log.Fatalf("Error walking through benchmark directory: %v", err)
 	}
 
-	return controls
+	return benchmarks
 }
 
-// parseBenchmarkFile parses a YAML file into a slice of Benchmark structs
+// parseBenchmarkFile parses a YAML file into a Benchmark struct
 func parseBenchmarkFile(filePath string) Benchmark {
 	file, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatalf("Error reading benchmark file: %v", err)
 	}
 
-	var benchmarks Benchmark
-	err = yaml.Unmarshal(file, &benchmarks)
+	var benchmark Benchmark
+	err = yaml.Unmarshal(file, &benchmark)
 	if err != nil {
 		log.Fatalf("Error unmarshalling benchmark YAML: %v", err)
 	}
 
-	return benchmarks
+	return benchmark
 }
 
 // getControls parses all control YAML files and returns a list of control IDs
-func getControls(root string) []string {
+func getControls(root string) ([]string, error) {
 	var controls []string
 
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
@@ -94,7 +98,10 @@ func getControls(root string) []string {
 			return err
 		}
 		if strings.HasSuffix(info.Name(), ".yaml") {
-			control := parseControlFile(path)
+			control, err := parseControlFile(path)
+			if err != nil {
+				return err
+			}
 			if control.ID != "" {
 				controls = append(controls, control.ID)
 			}
@@ -103,45 +110,69 @@ func getControls(root string) []string {
 	})
 
 	if err != nil {
-		log.Fatalf("Error walking through controls directory: %v", err)
+		return nil, err
 	}
 
-	return controls
+	return controls, nil
 }
 
 // parseControlFile parses a YAML file into a Control struct
-func parseControlFile(filePath string) Control {
+func parseControlFile(filePath string) (Control, error) {
 	file, err := os.ReadFile(filePath)
 	if err != nil {
-		log.Fatalf("Error reading control file: %v", err)
+		return Control{}, err
 	}
 
 	var control Control
 	err = yaml.Unmarshal(file, &control)
 	if err != nil {
-		log.Fatalf("Error unmarshalling control YAML: %v", err)
+		return Control{}, fmt.Errorf(fmt.Sprintf("control %s does not have the right structure", filePath))
 	}
 
-	return control
+	return control, nil
 }
 
-// compareControls compares the benchmark controls with the controls list and prints differences
-func compareControls(benchmarkControls, controlList []string) {
+// compareBenchmarks compares benchmarks' controls and children, and prints differences
+func compareBenchmarks(benchmarks map[string]Benchmark, controlList []string) int {
 	benchmarkControlSet := make(map[string]bool)
 	controlSet := make(map[string]bool)
+	benchmarkSet := make(map[string]bool)
+	var missingControl []string
+	var missingChild []string
 
-	for _, control := range benchmarkControls {
-		benchmarkControlSet[control] = true
-	}
-
+	// Populate controlSet and benchmarkSet
 	for _, control := range controlList {
 		controlSet[control] = true
 	}
+	for id := range benchmarks {
+		benchmarkSet[id] = true
+	}
 
-	fmt.Println(bold + "Controls in benchmarks but not in controls:" + reset)
-	for control := range benchmarkControlSet {
-		if !controlSet[control] {
-			fmt.Println(string(green) + " + " + string(reset) + control)
+	// Check controls and children
+	for _, benchmark := range benchmarks {
+		for _, control := range benchmark.Controls {
+			benchmarkControlSet[control] = true
+			if !controlSet[control] {
+				missingControl = append(missingControl, control)
+			}
+		}
+
+		for _, child := range benchmark.Children {
+			if !benchmarkSet[child] {
+				missingChild = append(missingChild, child)
+			}
+		}
+	}
+	if len(missingControl) > 0 {
+		fmt.Println(bold + "You need to add these controls to be able to merge to the main branch" + reset)
+		for _, control := range missingControl {
+			fmt.Println(string(green) + " + " + string(reset) + "Missing control " + control)
+		}
+	}
+	if len(missingChild) > 0 {
+		fmt.Println(bold + "You need to ensure all child benchmarks exist to be able to merge to the main branch" + reset)
+		for _, child := range missingChild {
+			fmt.Println(string(green) + " + " + string(reset) + "Missing child benchmark: " + child)
 		}
 	}
 
@@ -151,4 +182,10 @@ func compareControls(benchmarkControls, controlList []string) {
 			fmt.Println(string(red) + " - " + string(reset) + control)
 		}
 	}
+
+	if len(missingControl) > 0 || len(missingChild) > 0 {
+		return 1
+	}
+
+	return 0
 }
